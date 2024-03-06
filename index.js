f4988069420277472188d5ae79c7d5b3f64e8905
= <=>
2024-03-05 20:12:28 -0800

    parallax done

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var scrollPos = $(this).scrollTop();
-        $('.parallax-section').each(function() {
-            var sectionTop = $(this).offset().top;
-            var sectionHeight = $(this).outerHeight();
-            var opacity = 1 - (scrollPos - sectionTop) / sectionHeight;
-            if (opacity > 0) {
-                $(this).css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
-            } else {
-                $(this).css('background-color', 'transparent');
-            }
-        });
-    });
+// content array for easy editing
+
+const content = [
+  {
+        "id":"1",
+        "align": "c",
+        "title": "LatinX Civil Rights",
+        "contentHTML": ""
+    },
+   {
+        "id":"2",
+        "align": "l",
+        "title": "example title and stuff 1",
+        "contentHTML": "example description lorem ipsum dolor sit amet"
+    },
+    {
+        "id":"3",
+        "align": "r",
+        "title": "example title and stuff 2",
+        "contentHTML": "example description lorem ipsum dolor sit amet"
+    },
+    {
+        "id":"4",
+        "align": "l",
+        "title": "example title and stuff 3",
+        "contentHTML": "example description lorem ipsum dolor sit amet"
+    },
+    {
+        "id":"5",
+        "align": "r",
+        "title": "example title and stuff 4",
+        "contentHTML": "example description lorem ipsum dolor sit amet"
+    },
+    
+]
+content.forEach((slide) => {
+    const titleElement = document.getElementById(`title-${slide.id}`);
+    const contentElement = document.getElementById(`content-${slide.id}`);
+    const sectionElement = titleElement.closest('div'); // Get the parent section element
+    titleElement.innerText = slide.title;
+    contentElement.innerHTML = slide.contentHTML;
+
+    if (slide.align === 'l') {
+        sectionElement.style.transform = 'translateX(-125%)';
+    } else if (slide.align === 'r') {
+        sectionElement.style.transform = 'translateX(25%)';
+    } else {
+     
+    }
 });
